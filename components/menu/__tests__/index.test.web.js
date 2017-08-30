5fdc6d69912bed26a25366438e34b06737584b5b
maidu.sz <maidu.sz@alibaba-inc.com>
2017-08-30 18:03:44 +0800

    remove useless timefaker in menu test case

--- components/menu/__tests__/index.test.web.js
+++ components/menu/__tests__/index.test.web.js
@@ -37,12 +37,10 @@ describe('Menu', () => {
   });
 
   it('fires change event when item is selected', () => {
-    jest.useFakeTimers();
     const handleChange = jest.fn();
     const wrapper = mount(<Menu data={data} onChange={handleChange} />);
     wrapper.find('.am-list-item').at(1).simulate('click');
     wrapper.find('.am-list-item').at(3).find('.am-radio-input').simulate('change');
-    jest.runAllTimers();
     expect(handleChange).toBeCalledWith(['2', '22']);
   });
 
@@ -58,11 +56,9 @@ describe('Menu', () => {
     });
 
     it('fires change event when item is selected', () => {
-      jest.useFakeTimers();
       const handleChange = jest.fn();
       const wrapper = mount(<Menu data={data} level="1" onChange={handleChange} />);
       wrapper.find('.am-list-item').at(1).find('.am-radio-input').simulate('change');
-      jest.runAllTimers();
       expect(handleChange).toBeCalledWith(['2']);
     });
   });
@@ -81,13 +77,11 @@ describe('Menu', () => {
     });
 
     it('fires change event when item is selected', () => {
-      jest.useFakeTimers();
       const handleChange = jest.fn();
       const wrapper = mount(<Menu data={data} multSelect onChange={handleChange} />);
       wrapper.find('.am-list-item').at(1).simulate('click');
       wrapper.find('.am-list-item').at(2).find('.am-checkbox-input').simulate('change');
       wrapper.find('.am-list-item').at(3).find('.am-checkbox-input').simulate('change');
-      jest.runAllTimers();
       expect(handleChange).toHaveBeenLastCalledWith(['2', '21', '22']);
     });
   });
@@ -105,30 +99,24 @@ describe('Menu', () => {
     });
 
     it('fires change event when item is selected', () => {
-      jest.useFakeTimers();
       const handleChange = jest.fn();
       const wrapper = mount(<Menu data={data} multSelect level="1" onChange={handleChange} />);
       wrapper.find('.am-list-item').at(0).find('.am-checkbox-input').simulate('change');
       wrapper.find('.am-list-item').at(1).find('.am-checkbox-input').simulate('change');
-      jest.runAllTimers();
       expect(handleChange).toHaveBeenLastCalledWith(['1', '2']);
     });
 
     it('fires onOk event when ok btn is clicked', () => {
-      jest.useFakeTimers();
       const onOk = jest.fn();
       const wrapper = mount(<Menu data={data} multSelect value={['1', '2']} level="1" onOk={onOk} />);
       wrapper.find('.am-mult-select-btns-btn').at(1).simulate('click');
-      jest.runAllTimers();
       expect(onOk).toBeCalledWith(['1', '2']);
     });
 
     it('fires onCancel event when cancel btn is clicked', () => {
-      jest.useFakeTimers();
       const onCancel = jest.fn();
       const wrapper = mount(<Menu data={data} multSelect value={['1', '2']} level="1" onCancel={onCancel} />);
       wrapper.find('.am-mult-select-btns-btn').at(0).simulate('click');
-      jest.runAllTimers();
       expect(onCancel).toHaveBeenCalled();
     });
   });
